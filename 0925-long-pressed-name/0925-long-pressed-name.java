73f8555f8481ad9390d7d945bfa4e07442f4a5fb
Poovitha54 <poovitharaja4@gmail.com>
2026-01-18 10:48:15 +0530

    Time: 1 ms (80.55%), Space: 43 MB (43.99%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.java
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -1,13 +1,18 @@
-class Solution {
+class Solution{
     public boolean isLongPressedName(String name,String typed){
-    int i=0;
-    for (int j=0;j<typed.length();j++) {
-    if (i<name.length()&&name.charAt(i)==typed.charAt(j)){
-        i++;
-    } else if(j==0||typed.charAt(j)!=typed.charAt(j-1)){
-        return false;
-    }
-}
-return i==name.length();    
+        int i=0,j=0;
+        while(j<typed.length()){
+            if (i<name.length()&&name.charAt(i)==typed.charAt(j)){
+                i++;
+                j++;
+            }
+            else if(j>0&&typed.charAt(j)==typed.charAt(j-1)){
+                j++;
+            }
+            else{
+                return false;
+            }
+        }
+        return i==name.length();
     }
 }
